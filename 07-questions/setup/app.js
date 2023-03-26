78703598986b708baa58cdde388ebfe5b1004bb8
sil <dododot0128@gmail.com>
2023-03-26 17:50:39 +0900

    js stydy test

---
+++ 07-questions/setup/app.js
@@ -0,0 +1,19 @@
+//using selectors inside the element
+// traversing the dom
+
+const question = document.querySelectorAll(".question");
+
+question.forEach(function(orange){
+
+})
+
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach( function (btn){
+//   btn.addEventListener("click",function(e){
+//     const question = e.currentTarget.parentElemont.parentElemont
+//     question.classList.toggle("show-text");
+//     });
+
+//   });
